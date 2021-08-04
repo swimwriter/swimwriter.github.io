1279a0d596957ddb043c2563e7de300c196b73fb
ranjanbaag <42670091+ranjanbaag@users.noreply.github.com>
2021-08-04 11:33:51 +0530

    Create uploader.js

---
+++ uploader.js
@@ -0,0 +1,15 @@
+var myDropzone = new Dropzone(document.getElementById('dropzone-area'), {
+	uploadMultiple: false,
+	acceptedFiles:'.jpg,.png,.jpeg,.gif',
+	parallelUploads: 6,
+  url: 'https://api.cloudinary.com/v1_1/epadtool/image/upload'
+});
+
+myDropzone.on('sending', function (file, xhr, formData) {
+	formData.append('api_key', 869149651216927);
+	formData.append('timestamp', Date.now() / 1000 | 0);
+	formData.append('upload_preset', 'uoeqfpy3');
+});
+myDropzone.on('success', function (file, response) {
+	console.log('Success! Cloudinary public ID is', response.public_id);
+});
